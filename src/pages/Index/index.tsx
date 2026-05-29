00d26edb7bd7f5201c5ba1706940fc4f4cc4317a
chanweiyan <chanweiyan007@gmail.com>
2026-05-30 03:18:46 +0800

    fix: 显示头像

--- src/pages/Index/index.tsx
+++ src/pages/Index/index.tsx
@@ -3,13 +3,18 @@ import { Link, Outlet } from "react-router-dom";
 import "./index.css";
 
 export function Index() {
+  const userInfo = JSON.parse(localStorage.getItem("userInfo") || "{}");
   return (
     <div id="index-container">
       <div className="header">
         <h1>聊天室</h1>
 
         <Link to="/update_info">
-          <UserOutlined className="icon" />
+          {userInfo.headPic ? (
+            <img src={userInfo.headPic} alt="头像" className="avatar" width={60} height={60} />
+          ) : (
+            <UserOutlined className="icon" />
+          )}
         </Link>
       </div>
       <div className="body">
