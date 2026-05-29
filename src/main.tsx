40f2a83334484b07d96fd49d77a71e40a7356118
chanweiyan <chanweiyan007@gmail.com>
2026-05-29 20:05:41 +0800

    fix: add routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,30 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { createRoot } from "react-dom/client";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Login from "@/pages/Login";
+import Register from "@/pages/Register";
+import UpdatePassword from "@/pages/UpdatePassword";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const routes = [
+  {
+    path: "/",
+    element: <div>index</div>,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+  {
+    path: "/update_password",
+    element: <UpdatePassword />,
+  },
+];
+
+const router = createBrowserRouter(routes);
+
+const root = createRoot(document.getElementById("root")!);
+
+root.render(<RouterProvider router={router} />);
---
+++ src/pages/UpdatePassword/index.tsx
@@ -0,0 +1,9 @@
+function UpdatePassword() {
+  return (
+    <div>
+      <h1>Update Password</h1>
+    </div>
+  );
+}
+
+export default UpdatePassword;
